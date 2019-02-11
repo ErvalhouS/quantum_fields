510633c3ef5163d276c8bd99d290016fd2137a80
Fernando Bellincanta <ervalhous@hotmail.com>
2019-02-11 18:49:58 -0300

    Changing structure into gemified lib

--- lib/quantum_fields.rb
+++ lib/quantum_fields.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require 'active_support/concern'
+require 'quantum_fields/active_record_patch'
+require 'quantum_fields/no_sqlize'
 require 'quantum_fields/railtie'
 require 'quantum_fields/validation_injector'
-require 'quantum_fields/no_sqlize'
-require 'quantum_fields/active_record_patch'
 
 # A module to abstract a noSQL aproach into SQL records, using a `parameters`
 # JSON column.
@@ -16,10 +16,17 @@ module QuantumFields
     def no_sqlize(args = {})
       class_eval <<-RUBY, __FILE__, __LINE__+1
         def self.fields_column
-          :#{args[:fields_column] || :parameters}
+          :#{args[:fields_column] || :quantum_fields}
         end
         def self.rules_column
-          :#{args[:rules_column] || :parameters_rules}
+          :#{args[:rules_column] || :quantum_rules}
+        end
+        def as_json(*args)
+          super.except(self.class.fields_column.to_s, self.class.rules_column.to_s).tap do |hash|
+            self.try(:quantum_fields)&.each do |key, value|
+              hash[key] = value
+            end
+          end
         end
       RUBY
       include QuantumFields::NoSqlize
