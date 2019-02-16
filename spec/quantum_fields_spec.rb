7a959fc38ce1972a6a941d7f0eb1adbf29e3c212
Abinoam Praxedes Marques Junior <abinoam@gmail.com>
2019-02-15 22:27:56 -0300

    Add first actual spec for #no_sqlize

--- spec/quantum_fields_spec.rb
+++ spec/quantum_fields_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 require 'quantum_fields'
 
 RSpec.describe QuantumFields do
-  it { expect(true).to be_truthy }
+  it { expect(ActiveRecord::Base).to respond_to(:no_sqlize) }
 end
